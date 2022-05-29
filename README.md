9e56216484f8987fa816fea2064d7c3127b806a7
Aleksey Sokolov <sokolov.alexey@me.com>
2022-05-29 23:49:32 +0300

    initial commit - login module

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/a5qpukdo3xf0houn/branch/main?svg=true)](https://ci.appveyor.com/project/Sapogoha/ahj-8-chat-back/branch/main)
+
+# Chat - Backend
+
+Практическое задание после лекции "EventSource, Websockets"
+
+[Heroku](https://github.com/Sapogoha/ahj-8-chat-back)
---
+++ server.js
@@ -0,0 +1,82 @@
+const http = require('http');
+const Koa = require('koa');
+const WS = require('ws');
+const cors = require('@koa/cors');
+const { v4: uuid } = require('uuid');
+const app = new Koa();
+
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+    'Access-Control-Allow-Origin': true,
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
+  })
+);
+
+const port = process.env.PORT || 7070;
+const server = http.createServer(app.callback());
+const wsServer = new WS.Server({ server });
+const users = [];
+
+wsServer.on('connection', (ws) => {
+  ws.on('message', (msg) => {
+    const command = JSON.parse(msg);
+
+    if (command.event === 'newUser') {
+      const hasUsername = users.find(
+        (user) => user.username === command.message
+      );
+
+      if (hasUsername) {
+        ws.send(JSON.stringify({ event: 'error', error: 'taken' }));
+      } else {
+        const username = command.message;
+        const id = uuid();
+
+        users.push({ username, id, connection: ws });
+        const userList = users.map((user) => user.username);
+        const idList = users.map((user) => user.id);
+
+        ws.send(
+          JSON.stringify({ event: 'connect', users: userList, ids: idList })
+        );
+
+        Array.from(wsServer.clients)
+          .filter((o) => o.readyState === WS.OPEN)
+          .forEach((o) => {
+            o.send(
+              JSON.stringify({
+                event: 'notification - entered',
+                notification: `User ${username} entered the chat`,
+                newUser: username,
+                id: id,
+              })
+            );
+          });
+      }
+    }
+  });
+
+  ws.on('close', () => {
+    const left = users.find((user) => user.connection === ws);
+    const leftIndex = users.findIndex(
+      (user) => user.username === left.username
+    );
+    users.splice(leftIndex, 1);
+    Array.from(wsServer.clients)
+      .filter((o) => o.readyState === WS.OPEN)
+      .forEach((o) => {
+        o.send(
+          JSON.stringify({
+            event: 'notification - left',
+            notification: `User ${left.username} left the chat`,
+            leftUser: left.username,
+            id: left.id,
+          })
+        );
+      });
+  });
+});
+
+server.listen(port, () => console.log('Server started'));
